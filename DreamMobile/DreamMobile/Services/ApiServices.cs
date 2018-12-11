cdde5c40ba33778294ffeb091fa3bdf9d28e178e
Bohdan Solovyov <solovey2wingo@mail.ru>
2018-12-11 03:09:22 +0200

    sign in added

---
+++ DreamMobile/DreamMobile/Services/ApiServices.cs
@@ -0,0 +1,33 @@
+﻿using Dream.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Dream.Services
+{
+    public class ApiServices
+    {
+        internal async Task<bool> RegisterAsynk(string email, string password, string confirmPassword)
+        {
+            var client = new HttpClient();
+            var model = new RegisterBindingModel
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            HttpContent content = new StringContent(json);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            HttpResponseMessage response = await client.PostAsync("http://localhost:2273/api/account/register", content);
+
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
